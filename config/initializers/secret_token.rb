26322677b049512d7362d440951a89cf471a26e9
Matt gonzalez <mattgon50@gmail.com>
2014-07-29 01:03:52 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Enterprisecreator::Application.config.secret_key_base = '83b09bca9ac1a57fc569250c228f5c9906e6bf18d0c50e822b2ae175d14a6ed266faf442edd722704c66ecde83368661bf8229f5253d8962bb3695a3dff01013'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Enterprisecreator::Application.config.session_store :cookie_store, key: '_enterprisecreator_session'
